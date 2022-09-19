0b116c98857da33d31587289cc969573aea5e466
krdondon <1571600+oklokl@users.noreply.github.com>
2022-09-19 14:37:48 +0900

    fmedia 사용해서 음악 넣기
    fmedia "d:\NiceAndEasy.mp3" --background  한줄 하단에 추가 하는 버전 별거 없음 ㅎㅎ;;
    https://cafe.daum.net/candan/GGFN/337 참고 하세요.

---
+++ v8.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+REM 파일 입력 하여 변환하기
+cd /d "%~dp0">ddd1.txt
+for /f "delims=" %%f in ('type %*') do (
+echo echo "%%~f">>ddd1.txt
+)
+
+REM 배치 파일 만들기
+echo @echo off > temp.txt
+echo setlocal >> temp.txt
+type ddd1.txt >> temp.txt
+
+REM 음악 추가 https://cafe.daum.net/candan/GGFN/337 다운로드 받는 방법 여기 참조 하세요.
+REM fmedia 설치 되어 있어야 하고 NiceAndEasy.mp3 파일이 D 드라이브에 있어야 해요.
+REM NiceAndEasy.mp3 원하시는 파일 이름으로 바꾸시면 되네요. 자신이 원하는거 다운로드.
+echo fmedia "d:\NiceAndEasy.mp3" --background >> temp.txt
+move /y temp.txt test.bat
+
+pause
